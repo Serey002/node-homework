1a55aaa080fe3142005ab21833eeb9911ca65e9d
Serey002 <serey.phem.12022006@gmail.com>
2026-04-27 16:35:24 +0700

    Connect Database

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+//Database connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node_homework",
+    //My Mysql run on port 3307
+    port: 3307
+})
+
+//Make sure that database is connected or doesn't connected
+db.connect((err) => {
+    if(err){
+        console.log("Database connection failed", err);
+        return;
+    }else{
+        console.log("Database connected successully");
+    }
+})
