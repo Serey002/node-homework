df35528e13f026f094ef683236e5d87d8177750b
Serey002 <serey.phem.12022006@gmail.com>
2026-04-27 21:40:58 +0700

    create GRUD api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
+const { resourceLimits } = require("node:worker_threads");
 
 const app = express();
 
@@ -26,6 +27,117 @@ db.connect((err) => {
     }
 })
 
+//Get all users
+app.get("/users", (req, res) => {
+    db.query("SELECT * FROM users", (err, result) => {
+        if(err){
+            res.json({
+                err: true,
+                message: err
+            });
+        }else{
+            res.json({
+                list: result
+            });
+        }
+    })
+})
+
+//Create user
+app.post("/users", (req, res) => {
+    const {name} = req.body;
+    db.query("INSERT INTO users (name) VALUES (?)", [name], (err, result) => {
+        if(err){
+            res.status(500).json({               
+                err: true,
+                message: err
+            })
+        }else{
+            res.json({
+                message: "User created successfully",
+                id: result.insertId
+            });
+        }
+    });
+})
+
+
+//Get user by id
+app.get("/users/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    db.query(
+        "SELECT * FROM users WHERE id = ?",
+        [id],
+        (err, result) => {
+
+            if (err) {
+                return res.status(500).json({
+                    err: true,
+                    message: err
+                });
+            }
+
+            res.json({
+                user: result
+            });
+        }
+    );
+});
+
+// Update user
+app.put("/users/:id", (req, res) => {
+
+    const id = req.params.id;
+    const { name } = req.body;
+
+    db.query(
+        "UPDATE users SET name = ? WHERE id = ?",
+        [name, id],
+        (err, result) => {
+
+            if (err) {
+                return res.status(500).json({
+                    err: true,
+                    message: err
+                });
+            }
+
+            res.json({
+                message: "User updated successfully"
+            });
+
+        }
+    );
+
+});
+
+//Delete
+app.delete("/users/:id", (req, res) => {
+
+    const id = req.params.id;
+
+    db.query(
+        "DELETE FROM users WHERE id = ?",
+        [id],
+        (err, result) => {
+
+            if (err) {
+                return res.status(500).json({
+                    err: true,
+                    message: err
+                });
+            }
+
+            res.json({
+                message: "User deleted successfully"
+            });
+
+        }
+    );
+
+});
 
 app.listen(3000, () => {
     console.log("Server is run on port http://localhost:3000");
